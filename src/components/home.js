37f896b245b43f26c07a1292189ef12cb284265e
Jasmin Trotter <jasminmtrotter@gmail.com>
2018-09-12 22:36:43 -0700

    fixed typo on homepage

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ export default function home(props) {
 		<div>
 			<section>
       			<h3>About Us</h3>
-      			<p><a href="https://www.balletbodybyjasmin.com" target="_blank">Ballet Body by Jasmin</a> is an adult ballet program for absoulte beginners. Create an account <a href="#">here</a> to purchase a class or a package of multiple classes. You will also be able to view a history of your purchases on your dashboard. See you in class!</p>
+      			<p><a href="https://www.balletbodybyjasmin.com" target="_blank">Ballet Body by Jasmin</a> is an adult ballet program for absolute beginners. Create an account <a href="#">here</a> to purchase a class or a package of multiple classes. You will also be able to view a history of your purchases on your dashboard. See you in class!</p>
     		</section>
 		</div>
 	);
